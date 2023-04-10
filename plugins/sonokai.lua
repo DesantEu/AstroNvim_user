837371b6cc5f06986f25b3b4ff74f9349121768a
DesantEu <wazzap203203@list.ru>
2023-04-11 01:36:37 +0200

    removed other themes

--- plugins/sonokai.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sainnhe/sonokai",
-  init = function() -- init function runs before the plugin is loaded
-    vim.g.sonokai_style = "shusia"
-  end,
-  -- opts = {
-  --   dimInactive = { enabled = true, percentage = 0.25 },
-  -- }
-}
